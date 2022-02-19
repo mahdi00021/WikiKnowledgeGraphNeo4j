09fe9db482e8c6339d686325caf40670e59d0cea
mahdi00021 <m73hdi@gmail.com>
2022-02-19 05:31:50 +0330

    change code

--- App/WikiKnowledgeGraph.php
+++ App/WikiKnowledgeGraph.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace App;
+
+error_reporting(1);
 
 use Laudis\Neo4j\Authentication\Authenticate;
 use Laudis\Neo4j\ClientBuilder;
@@ -10,71 +13,70 @@ class WikiKnowledgeGraph
     protected static $config = [];
     protected static $client;
 
+    public static function setConfig($config = [])
+    {
 
-	public static function setConfig($config = [])
-	{
-
-		self::$config = $config;
+        self::$config = $config;
         $auth = Authenticate::basic(self::$config['username'], self::$config['password']);
-        self::$client = ClientBuilder::create()->withDriver('default', self::$config['bolt'],$auth)->build();
-	}
-	
-	
-	public static function runGraphCategory()
-	{
+        self::$client = ClientBuilder::create()
+            ->withDriver('default', self::$config['bolt'], $auth)
+            ->build();
+
+    }
+
+
+    public static function runGraphCategory()
+    {
 
         self::$client->run('CREATE INDEX ON :Category(catId)');
         self::$client->run('CREATE INDEX ON :Page(pageTitle)');
         self::$client->run('CREATE INDEX ON :Category(catName)');
-        self::$client->run('CREATE (c:Category:RootCategory {catId: 0, catName: '.self::$config['rootcategory'].', subcatsFetched : false, pagesFetched : false, level: 0 }');
-		
-		$query = "
-			    UNWIND range(0,3) as level
-				CALL apoc.cypher.doIt('
+        self::$client->run('CREATE (c:Category:RootCategory {catId: 0, catName: ' . self::$config['rootcategory'] . ', subcatsFetched : false, pagesFetched : false, level: 0 }');
+
+        $query = '
+			    UNWIND range(0,2) as level
 				MATCH (c:Category { subcatsFetched: false, level: $level})
-				CALL apoc.load.json(".self::$config['hosturl']."/w/api.php?format=json&action=query&list=categorymembers&cmtype=subcat&cmtitle=Category:' + apoc.text.urlencode(c.catName) + '&cmprop=ids|title&cmlimit=500')
+				CALL apoc.load.json("https://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers&cmtype=subcat&cmtitle=Category:" + apoc.text.urlencode(c.catName) + "&cmprop=ids|title&cmlimit=500")
 				YIELD value as results
 				UNWIND results.query.categorymembers AS subcat
 				MERGE (sc:Category {catId: subcat.pageid})
 				ON CREATE SET sc.catName = substring(subcat.title,9),
 							  sc.subcatsFetched = false,
 							  sc.pagesFetched = false,
-							  sc.level = $level + 1
+							  sc.level = level + 1
 				WITH sc,c
-				CALL apoc.create.addLabels(sc,['Level' +  ($level + 1) + 'Category']) YIELD node
+				CALL apoc.create.addLabels(sc,["Level" +  (level + 1) + "Category"]) YIELD node
 				MERGE (sc)-[:SUBCAT_OF]->(c)
 				WITH DISTINCT c
-				SET c.subcatsFetched = true', { level: level }) YIELD value
-				RETURN value
-		";
-		
-		$categorys = self::$client->run($query);
-		
-		return $categorys;
-			
-	}
-	
-	public static function runGraphPages()
-	{
+				SET c.subcatsFetched = true
+		';
 
-		$query = "
-			UNWIND range(0,4) as level
-			CALL apoc.cypher.doIt('
-			MATCH (c:Category { pagesFetched: false, level: $level })
-			CALL apoc.load.json(".self::$config['hosturl']."/w/api.php?format=json&action=query&list=categorymembers&cmtype=page&cmtitle=Category:' + apoc.text.urlencode(c.catName) + '&cmprop=ids|title&cmlimit=500')
-			YIELD value as results
-			UNWIND results.query.categorymembers AS page
-			MERGE (p:Page {pageId: page.pageid})
-			ON CREATE SET p.pageTitle = page.title, p.pageUrl = ".self::$config['hosturl']."/wiki/ + apoc.text.urlencode(replace(page.title, ' ', '_'))
-			WITH p,c
-			MERGE (p)-[:IN_CATEGORY]->(c)
-			WITH DISTINCT c
-			SET c.pagesFetched = true', { level: level }) yield value
-			return value
-		";
-	
-	    $result = self::$client->run($query);
-		
+        $categorys = self::$client->run($query)->getResult();
+
+        return $categorys;
+
+    }
+
+    public static function runGraphPages()
+    {
+
+        $query =
+			'
+			    UNWIND range(0,2) as level
+                MATCH (c:Category { pagesFetched: false, level: level })
+                CALL apoc.load.json("https://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers&cmtype=page&cmtitle=Category:Cars&cmprop=ids|title&cmlimit=10")
+                YIELD value as results
+                UNWIND results.query.categorymembers AS page
+                MERGE (p:Page {pageId: page.pageid})
+                ON CREATE SET p.pageTitle = page.title, p.pageUrl = "https://en.wikipedia.org/wiki/" + apoc.text.urlencode(replace(page.title, " ", "_"))
+                WITH p,c
+                MERGE (p)-[:IN_CATEGORY]->(c)
+                WITH DISTINCT c
+                SET c.pagesFetched = true
+                ';
+
+        $result = self::$client->run($query);
+
 		return $result;
 	}
 
@@ -82,24 +84,23 @@ class WikiKnowledgeGraph
 	public static function runGraphPagesWithoutCategory()
 	{
 
-		$query = "
-			UNWIND range(0,4) as level
-			CALL apoc.cypher.doIt('
-			MATCH (c:Category { pagesFetched: false, level: $level })
-			CALL apoc.load.json(".self::$config['hosturl']."/w/api.php?action=query&format=json&list=allpages&aplimit=max')
+		$query = '
+			UNWIND range(0,2) as level
+			MATCH (c:Category { pagesFetched: false, level: level })
+			CALL apoc.load.json("https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&aplimit=max")
 			YIELD value as results
 			UNWIND results.query.allpages AS page
 			MERGE (p:Page {pageId: page.pageid})
-			ON CREATE SET p.pageTitle = page.title, p.pageUrl = ".self::$config['hosturl']."/wiki/ + apoc.text.urlencode(replace(page.title, ' ', '_'))
+			ON CREATE SET p.pageTitle = page.title, p.pageUrl = "https://en.wikipedia.org/wiki/" + apoc.text.urlencode(replace(page.title, " ", "_"))
 			WITH p,c
-			MERGE (p)-[:PAGE_ROOT]->(c)
+			MERGE (p)-[:IN_CATEGORY]->(c)
 			WITH DISTINCT c
-			SET c.pagesFetched = true', { level: level }) yield value
-			return value
-		";
+			SET c.pagesFetched = true
+		';
 		
 		$result = self::$client->run($query);
 		
 		return $result;
 	}
+
 }
