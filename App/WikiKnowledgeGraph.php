24d8c85b32cc9b2b5c5b70f90928aab2a2fbd3e6
mahdi00021 <m73hdi@gmail.com>
2022-02-19 15:33:12 +0330

    change code php

--- App/WikiKnowledgeGraph.php
+++ App/WikiKnowledgeGraph.php
@@ -31,27 +31,27 @@ class WikiKnowledgeGraph
         self::$client->run('CREATE INDEX ON :Category(catId)');
         self::$client->run('CREATE INDEX ON :Page(pageTitle)');
         self::$client->run('CREATE INDEX ON :Category(catName)');
-        self::$client->run('CREATE (c:Category:RootCategory {catId: 0, catName: Cars, subcatsFetched : false, pagesFetched : false, level: 0 }');
+        self::$client->run("CREATE (c:Category:RootCategory {catId: 0, catName: '" . self::$config['rootcategory'] . "', subcatsFetched : false, pagesFetched : false, level: 0 })");
 
-        $query = '
-			    UNWIND range(0,2) as level
-				MATCH (c:Category { subcatsFetched: false, level: $level})
-				CALL apoc.load.json("https://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers&cmtype=subcat&cmtitle=Category:" + apoc.text.urlencode(c.catName) + "&cmprop=ids|title&cmlimit=500")
-				YIELD value as results
-				UNWIND results.query.categorymembers AS subcat
-				MERGE (sc:Category {catId: subcat.pageid})
-				ON CREATE SET sc.catName = substring(subcat.title,9),
-							  sc.subcatsFetched = false,
-							  sc.pagesFetched = false,
-							  sc.level = level + 1
-				WITH sc,c
-				CALL apoc.create.addLabels(sc,["Level" +  (level + 1) + "Category"]) YIELD node
-				MERGE (sc)-[:SUBCAT_OF]->(c)
-				WITH DISTINCT c
-				SET c.subcatsFetched = true
-		';
+        $query = 'UNWIND range(0,2) as level
+                    CALL apoc.cypher.doIt("
+                    MATCH (c:Category { subcatsFetched: false, level: $level})
+                    CALL apoc.load.json(\'https://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers&cmtype=subcat&cmtitle=Category:\' + apoc.text.urlencode(c.catName) + \'&cmprop=ids|title&cmlimit=500\')
+                    YIELD value as results
+                    UNWIND results.query.categorymembers AS subcat
+                    MERGE (sc:Category {catId: subcat.pageid})
+                    ON CREATE SET sc.catName = substring(subcat.title,9),
+                                  sc.subcatsFetched = false,
+                                  sc.pagesFetched = false,
+                                  sc.level = $level + 1
+                    WITH sc,c
+                    CALL apoc.create.addLabels(sc,[\'Level\' +  ($level + 1) + \'Category\']) YIELD node
+                    MERGE (sc)-[:SUBCAT_OF]->(c)
+                    WITH DISTINCT c
+                    SET c.subcatsFetched = true", { level: level }) YIELD value
+                    RETURN value';
 
-        $categorys = self::$client->run($query)->getResult();
+        $categorys = self::$client->run($query);
 
         return $categorys;
 
@@ -60,47 +60,46 @@ class WikiKnowledgeGraph
     public static function runGraphPages()
     {
 
-        $query =
-			'
-			    UNWIND range(0,2) as level
-                MATCH (c:Category { pagesFetched: false, level: level })
-                CALL apoc.load.json("https://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers&cmtype=page&cmtitle=Category:Cars&cmprop=ids|title&cmlimit=10")
-                YIELD value as results
-                UNWIND results.query.categorymembers AS page
-                MERGE (p:Page {pageId: page.pageid})
-                ON CREATE SET p.pageTitle = page.title, p.pageUrl = "https://en.wikipedia.org/wiki/" + apoc.text.urlencode(replace(page.title, " ", "_"))
-                WITH p,c
-                MERGE (p)-[:IN_CATEGORY]->(c)
-                WITH DISTINCT c
-                SET c.pagesFetched = true
-                ';
+        $query = 'UNWIND range(0,2) as level
+                    CALL apoc.cypher.doIt("
+                    MATCH (c:Category { pagesFetched: false, level: $level })
+                    CALL apoc.load.json(\'https://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers&cmtype=page&cmtitle=Category:\' + apoc.text.urlencode(c.catName) + \'&cmprop=ids|title&cmlimit=500\')
+                    YIELD value as results
+                    UNWIND results.query.categorymembers AS page
+                    MERGE (p:Page {pageId: page.pageid})
+                    ON CREATE SET p.pageTitle = page.title, p.pageUrl = \'http://en.wikipedia.org/wiki/\' + apoc.text.urlencode(replace(page.title, \' \', \'_\'))
+                    WITH p,c
+                    MERGE (p)-[:IN_CATEGORY]->(c)
+                    WITH DISTINCT c
+                    SET c.pagesFetched = true", { level: level }) yield value
+                    return value';
 
         $result = self::$client->run($query);
 
-		return $result;
-	}
+        return $result;
+    }
 
-	
-	public static function runGraphPagesWithoutCategory()
-	{
 
-		$query = '
-			UNWIND range(0,2) as level
-			MATCH (c:Category { pagesFetched: false, level: level })
-			CALL apoc.load.json("https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&aplimit=max")
-			YIELD value as results
-			UNWIND results.query.allpages AS page
-			MERGE (p:Page {pageId: page.pageid})
-			ON CREATE SET p.pageTitle = page.title, p.pageUrl = "https://en.wikipedia.org/wiki/" + apoc.text.urlencode(replace(page.title, " ", "_"))
-			WITH p,c
-			MERGE (p)-[:IN_CATEGORY]->(c)
-			WITH DISTINCT c
-			SET c.pagesFetched = true
-		';
-		
-		$result = self::$client->run($query);
-		
-		return $result;
-	}
+    public static function runGraphPagesWithoutCategory()
+    {
+
+        $query = 'UNWIND range(0,3) as level
+                    CALL apoc.cypher.doit("
+                    MATCH (c:Category { pagesFetched: false, level: $level })
+                    CALL apoc.load.json(\'https://en.wikipedia.org/w/api.php?action=query&format=json&list=allpages&aplimit=max\')
+                    YIELD value as results
+                    UNWIND results.query.categorymembers AS page
+                    MERGE (p:Page {pageId: page.pageid})
+                    ON CREATE SET p.pageTitle = page.title, p.pageUrl = \'http://en.wikipedia.org/wiki/\' + apoc.text.urlencode(replace(page.title, \' \', \'_\'))
+                    WITH p,c
+                    MERGE (p)-[:IN_CATEGORY]->(c)
+                    WITH DISTINCT c
+                    SET c.pagesFetched = true", { level: level }) yield value
+                    return value';
+
+        $result = self::$client->run($query);
+
+        return $result;
+    }
 
 }
